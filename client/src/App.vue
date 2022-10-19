8233a0d12d9010ab20922eafe024ac0a3b8d232c
Ubuntu <ubuntu@ip-172-31-40-20.ap-southeast-1.compute.internal>
2022-10-19 08:10:39 +0000

    feat: login

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <main>
-    <div class="login-card">
-      <DWButton size="large" @click="kickstart">DRINK WATER</DWButton>
+    <div class="start">
+      <Transition name="fade">
+        <DWButton v-show="showStart" size="large" @click="kickstart"
+          >DRINK WATER</DWButton
+        >
+      </Transition>
+      <Transition> </Transition>
     </div>
     <DWWaveTank ref="tank" :initialProgress="30" :showProgress="showProgress" />
   </main>
@@ -16,10 +21,14 @@ export default {
     return {
       progress: 0,
       showProgress: false,
+      showStart: true,
     };
   },
   methods: {
-    kickstart: function () {},
+    kickstart: function () {
+      this.showStart = false;
+      this.$refs.tank.setProgress(100);
+    },
     addWater: function () {
       this.progress += 5;
       this.$refs.tank.setProgress(this.progress);
@@ -29,11 +38,21 @@ export default {
 </script>
 
 <style>
-.login-card {
+.start {
   width: 80vw;
   height: 80vh;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
---
+++ client/src/components/DWLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login-card">
+    <p>{{ usernamePrompt }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usernamePrompt: "What is your name?",
+    };
+  },
+};
+</script>
+
+<style>
+.login-card {
+  width: 80%;
+  height: 80%;
+  border-radius: 20px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 0px 10px 0px;
+}
+</style>
