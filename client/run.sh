8f54d6c49baa46cd8b354a8a48bfeb5a40bd798d
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2022-10-20 17:43:41 +0800

    setup docker

---
+++ client/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it -dp 8081:80 --rm --name drink-water-client sutd-devtools/drink-water-client
---
+++ server/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it -dp 8080:80 --rm --name drink-water-server sutd-devtools/drink-water-server
