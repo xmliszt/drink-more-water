a4db75fb958ca51ddee92ae3ed8aec059cb6e934
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2022-10-20 15:29:38 +0800

    fix: add production environment

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,6 +16,11 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
       port: 8081,
     };
+  } else if (mode === "production") {
+    server = {
+      host: true,
+      port: 8081,
+    };
   }
   return {
     plugins: [vue()],
